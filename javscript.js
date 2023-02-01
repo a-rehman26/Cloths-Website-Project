98a2b3909cded080c4df18a6987f51cc0723ad90
A_rehman <abdul44@gmail.com>
2023-02-02 01:03:04 +0500

    Second commit may be FOR Header and Navbar Done in Desktop responsive

---
+++ javscript.js
@@ -0,0 +1,5 @@
+// wishlist alert navbar 
+
+function wishNav() {
+    alert("Nothing To Add in Wish Right Now")
+}
---
+++ jquery home.js
@@ -0,0 +1,29 @@
+// hide search navbar 
+
+$(document).ready(function () {
+    $("#navSearchright").click(function () {
+        $("#hideSearchdiv").fadeIn(function () {
+            $("#hideSearchdiv").css("display", "block");
+        });
+    });
+
+    $("#hidesearchinputicon").click(function () {
+        $("#hideSearchdiv").fadeOut(function () {
+            $("#hideSearchdiv").css("display", "none");
+        });
+    });
+
+    // hide navbar shopping cart
+    $("#cartShoppingnav").click(function () {
+        // $("#hideCartdiv").fadeIn(function () {
+        $("#hideCartdiv").css("transform", "translateX(0)")
+        // })
+    })
+
+    $("#cartCloseicon").click(function () {
+        // $("#hideCartdiv").fadeOut(function () {
+        $("#hideCartdiv").css("transform", "translateX(100%)")
+        // })
+    })
+});
+
