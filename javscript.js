b16695dbbafba87a9c8dad44eee691be438d271f
A_rehman <abdul44@gmail.com>
2023-02-02 22:40:34 +0500

    Third Commit for navbar Responsive and header and home page

--- javscript.js
+++ javscript.js
@@ -3,3 +3,19 @@
 function wishNav() {
     alert("Nothing To Add in Wish Right Now")
 }
+
+// navbar responsive
+
+function navRes() {
+    let nav_mob = document.querySelector(".icons-hide-res-div");
+
+    nav_mob.addEventListener("click", () => top_nav())
+
+    let header_nav = document.querySelector(".navbar-main-div");
+
+    top_nav = () => {
+        header_nav.classList.toggle("activeNavbarRes")
+    }
+
+
+}
