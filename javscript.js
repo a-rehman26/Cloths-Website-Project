99e6a62afdaade2b651989cfcdc6972fd8710c8f
A_rehman <abdul44@gmail.com>
2023-02-06 00:04:09 +0500

    Login Page work saved

--- javscript.js
+++ javscript.js
@@ -20,5 +20,55 @@ function navRes() {
 
 }
 
-// filter in product home cards
+// error login form inputs required
 
+
+
+function formSubF() {
+    let user = document.getElementById("userinpID").value;
+    let pass = document.getElementById("userpassID").value;
+
+    let errorP = document.getElementById("errorLoginFrm");
+    let errorPtwo = document.getElementById("errorLoginFrm2");
+
+    let userDiv = document.getElementById("user-div-error-bd-clr");
+    let passDiv = document.getElementById("pass-div-error-bd-clr");
+
+    let user_inp = document.forms["mloginFrm"]["userName"].value;
+    let pass_inp = document.forms["mloginFrm"]["userPass"].value;
+
+
+    if (user_inp === "") {
+        errorP.style.display = "block";
+        userDiv.style.borderColor = "red"
+        return false;
+    }
+    else {
+        errorP.style.display = "none";
+        userDiv.style.borderColor = "#222"
+
+    }
+
+    if (pass_inp === "") {
+        errorPtwo.style.display = "block";
+        passDiv.style.borderColor = "red"
+        return false;
+    }
+    else {
+        errorPtwo.style.display = "none";
+        passDiv.style.borderColor = "#222"
+
+        alert("Login Done")
+    }
+
+
+
+    // let user_Inp = "abdul11@gmail.com";
+    // let user_Pass = "abdulR123";
+
+    // if (user === user_Inp && pass === user_Pass) {
+    //     alert("Login Done");
+    // }
+
+
+}
